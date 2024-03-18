b22f851364b43b6fc0d8b2ce600ad4f838656e1d
w-winiarz <winiarzwojtek@gmail.com>
2024-03-18 22:09:26 +0100

    LAB3_ZADANIA

---
+++ main.py
@@ -0,0 +1,78 @@
+import math
+from math import *
+import sys
+
+# ZADANIE1
+from random import randint
+# a = [1-x for x in range(1,11)]
+# print(a)
+# b = [4**i for i in range(7)]
+# print(b)
+# c = [x for x in b if x % 2 == 0]
+# print(c)
+
+# a=[]
+# for x in range (1,11):
+#     a.append(1-x)
+# print(a)
+# b=[]
+# for y in range(7):
+#     b.append(4**y)
+# print (b)
+# random.randint(45, 55)
+# lista1=[]
+# for i in range(10):
+#     lista1.append(randint(1,1000))
+# print (lista1)
+# lista2 = [i for i in lista1 if i % 2 == 0]
+# print(lista2)
+# ZADANIE3
+# slownik={'mleko':'sztuki','jablka':'kg','czekolada':'sztuki'}
+# odwrocony_slownik={value:key for key,value in slownik.items() if value=="sztuki"}
+# print(slownik)
+# print(odwrocony_slownik)
+# ZADANIE4
+# def czy_trojkat_prostokatny(a,b,c):
+#     if(a*a+b*b==c*c):
+#         return True
+#     else:
+#         return False
+# a=3
+# b=4
+# c=5
+# print(czy_trojkat_prostokatny(a,b,c))
+# ZADANIE 5
+# def pole_trapezu(a=1,b=1,h=1):
+#     pole=((a+b)*h)/2
+#     return pole
+# a=5
+# b=6
+# h=8
+# print(pole_trapezu())
+# print(pole_trapezu(a,b,h))
+# ZADANIE 6
+# def ciag(a1,b,ile):
+#     i=1
+#     wynik=1
+#     for i in range(ile):
+#         wynik=wynik*a1
+#         a1 = a1 * b
+#     return wynik;
+# a1=1
+# b=4
+# ile=10
+# print(ciag(a1,b,ile))
+# ZADANIE 7
+# def pierwiastek(a):
+#     try:
+#         if(a>0):
+#             return a**(1/2)
+#         else:
+#             raise ValueError("Liczba musi być dodatnia")
+#     except ValueError:
+#         print("Błąd liczba musi być dodatnia")
+# a=-25
+# print(pierwiastek(a))
+
+
+
